ec4afcb03dd2cb28bc6d9b7c698f79c9b2e2cfd2
mhda7mn <230830129+mhda7mn@users.noreply.github.com>
2025-10-27 01:01:07 +0400

    variable fixes

--- src/functions/loadJson.ts
+++ src/functions/loadJson.ts
@@ -5,6 +5,7 @@ const loadJSON = async (path: string, c: Context) => {
 	const { BASE_URL } = env<{ BASE_URL: string }>(c);
 
 	const fullUrl = `${BASE_URL}${path}`;
+
 	const res = await fetch(fullUrl);
 	if (!res.ok) throw new Error("Not found");
 	return res.json();
