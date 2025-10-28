be10f716aca4357dde37cce31fcfddc79104e843
mhda7mn <230830129+mhda7mn@users.noreply.github.com>
2025-10-28 16:37:26 +0400

    refined api index response

--- README.md
+++ README.md
@@ -49,27 +49,27 @@ npm run deploy
 
 Once the API is up and running you can access the following endpoints
 
-### 1. GET /api/v1/surahs
+1. GET /v1/surahs
 
-### 2. GET /api/v1/surahs/:surahnumber
+2. GET /v1/surahs/:surahnumber
 
-### 3. GET /api/v1/ayahs/:surahnumber/:ayahnumber
+3. GET /v1/ayahs/:surahnumber/:ayahnumber
 
-### 4. GET /api/v1/juz
+4. GET /v1/juz
 
-### 5. GET /api/v1/juz/:juznumber
+5. GET /v1/juz/:juznumber
 
-### 6. GET /api/v1/hizb
+6. GET /v1/hizb
 
-### 7. GET /api/v1/hizb/:hizbnumber
+7. GET /v1/hizb/:hizbnumber
 
-### 8. GET /api/v1/pages
+8. GET /v1/pages
 
-### 9. GET /api/v1/pages/:pagenumber
+9. GET /v1/pages/:pagenumber
 
-### 10. GET /api/v1/tafseer/:surahnumber
+10. GET /v1/tafseer/:surahnumber
 
-### 11. GET /api/v1/tafseer/:surahbnumber/:ayahnumber
+11. GET /v1/tafseer/:surahbnumber/:ayahnumber
 
 ## Roadmap
 
--- src/index.tsx
+++ src/index.tsx
@@ -28,6 +28,41 @@ app.use("*", async (c, next) => {
 	return res;
 });
 
+app.get("/", (c) => {
+	return c.json({
+		status: 200,
+		documentation: "https://quranapi.com",
+		version: "1.0.0 (27-10-2025)",
+		api: {
+			v1: {
+				surahs: {
+					all: "/v1/surahs",
+					specific: "/v1/surahs/:surahnumber",
+				},
+				ayahs: {
+					specific: "/v1/surahs/:surahnumber/:ayahnumber",
+				},
+				juz: {
+					all: "/v1/juz",
+					specific: "/v1/juz/:juznumber",
+				},
+				hizb: {
+					all: "/v1/hizb",
+					specific: "/v1/hizb/:hizbnumber",
+				},
+				pages: {
+					all: "/v1/pages",
+					specific: "/v1/pages/:pagenumber",
+				},
+				tafseer: {
+					allSurah: "/v1/tafseer/:surahnumber",
+					specificAyah: "/v1/tafseer/:surahnumber/:ayahnumber",
+				},
+			},
+		},
+	});
+});
+
 app.route("/v1/surahs", surahRoutes);
 app.route("/v1/ayahs", ayahRoutes);
 app.route("/v1/juz", juzRoutes);
@@ -35,8 +70,8 @@ app.route("/v1/hizb", hizbRoutes);
 app.route("/v1/pages", pageRoutes);
 app.route("/v1/tafseer", tafseerRoutes);
 
-app.all("*", (c) => {
-	return c.json({ message: "Route not found" }, 404);
-});
+app.all("*", (c) =>
+	c.json({ status: 404, message: "Route not found" }, 404)
+);
 
 export default app;
